01438eab742323de4a0858b30e14f21ba7efae79
bacpd <pham.duc.bac@vsi-international.com>
2020-06-04 10:05:23 +0700

    task view blade 4-6-2020

---
+++ task view blade/index.blade.php
@@ -0,0 +1,30 @@
+@extends('master')
+
+@section('banner')
+    <div class="jumbotron text-center" style="margin-bottom:0">
+        <h1>My First Bootstrap 4 Page</h1>
+        <p>Resize this responsive page to see the effect!</p>
+    </div>
+@endsection
+
+@section('menu')
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+@endsection
---
+++ task view blade/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('header')
+<body>
+
+@yield('banner')
+
+@yield('menu')
+
+@include('content')
+
+@include('footer')
+</body>
+</html>
